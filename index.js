1ba3a3690e7fbabce5f7afbe2b52847ac7d02490
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-24 12:07:43 +0300

    creates method speak for class bird

--- index.js
+++ index.js
@@ -24,7 +24,10 @@ class Bird {
     this.name =name;
     this.sex = sex;
   }
-  //  speak(){
-  //   return `${this.name} says !`
-  // }
+  speak(){if (this.sex=== 'male'){
+    return `It's me! ${this.name}, the parrot!`
+  } else{
+    return `${this.name} says squawk!`
+  }
+  }
 }
